9ba01b7ef2f109fa71fca931fc3a6b0bfdbf0d81
MHR_171 <george_the@ymail.com>
2020-07-19 02:37:30 +0900

    first commit

---
+++ Assets/Script/CrewMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class CrewMovement : MonoBehaviour
+{
+    public AIPath m_aiPath;
+
+    Vector2 dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        faceVelocity();
+    }
+
+    void faceVelocity()
+    {
+        dir = m_aiPath.desiredVelocity;
+
+        transform.right = dir;
+    }
+}
